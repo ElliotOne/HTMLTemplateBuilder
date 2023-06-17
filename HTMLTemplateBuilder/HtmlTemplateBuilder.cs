b706e04ca20c19166a3ecccf96909df3ff4dff52
AMZEnterprise <alimomenzadeh78@gmail.com>
2023-06-17 11:18:33 +0330

    Add project files.

---
+++ HTMLTemplateBuilder/HtmlTemplateBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace HTMLTemplateBuilder
+{
+    class HtmlTemplateBuilder
+    {
+        private StringBuilder templateBuilder;
+
+        public HtmlTemplateBuilder()
+        {
+            templateBuilder = new StringBuilder();
+        }
+
+        public HtmlTemplateBuilder AddHeader(string title)
+        {
+            templateBuilder.Append($"<!DOCTYPE html><html><head><title>{title}</title></head><body>");
+            return this;
+        }
+
+        public HtmlTemplateBuilder AddParagraph(string text)
+        {
+            templateBuilder.Append($"<p>{text}</p>");
+            return this;
+        }
+
+        public HtmlTemplateBuilder AddList(string[] items)
+        {
+            templateBuilder.Append("<ul>");
+            foreach (string item in items)
+            {
+                templateBuilder.Append($"<li>{item}</li>");
+            }
+            templateBuilder.Append("</ul>");
+            return this;
+        }
+
+        public string Build()
+        {
+            templateBuilder.Append("</body></html>");
+            string template = templateBuilder.ToString();
+            templateBuilder.Clear();
+            return template;
+        }
+    }
+}
---
+++ HTMLTemplateBuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿using HTMLTemplateBuilder;
+
+HtmlTemplateBuilder builder = new HtmlTemplateBuilder();
+
+string htmlTemplate = builder
+    .AddHeader("My HTML Template")
+    .AddParagraph("This is a paragraph.")
+    .AddList(new string[] { "Item 1", "Item 2", "Item 3" })
+    .Build();
+
+Console.WriteLine(htmlTemplate);
+
+Console.ReadKey();
